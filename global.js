2c09f7431bb7babe8989390a9c46b780ead47695
matthewvaught <vaught.matthew05@gmail.com>
2025-10-14 21:21:13 -0700

    Update global.js

--- global.js
+++ global.js
@@ -19,6 +19,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' }
+  { url: 'https://github.com/matthew-vaught', title: 'Github'}
 ];
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
