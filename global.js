6d59edb9d668bb8ce1d7df0eb6d32b47090bb27c
matthewvaught <vaught.matthew05@gmail.com>
2025-10-14 21:06:27 -0700

    Update global.js

--- global.js
+++ global.js
@@ -32,5 +32,8 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
   url = !url.startsWith('http') ? BASE_PATH + url : url;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
 }
