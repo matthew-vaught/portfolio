7b5ed51283466edf3b019d2a8a9a84e362af36ca
matthewvaught <vaught.matthew05@gmail.com>
2025-10-29 21:59:12 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -18,22 +18,8 @@ let arc = arcGenerator({
 });
 
 let data = [1, 2];
-
-let total = 0;
-
-for (let d of data) {
-  total += d;
-}
-
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
-
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
 let colors = ['gold', 'purple'];
