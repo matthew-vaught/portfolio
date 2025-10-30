97747a7461c3607d7d283b35c2c7af6c995a0052
matthewvaught <vaught.matthew05@gmail.com>
2025-10-30 11:03:15 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -17,14 +17,16 @@ let arc = arcGenerator({
   endAngle: 2 * Math.PI,
 });
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
+
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
