70887cd1d92c0a68ed51a34a97159b8c6531fe30
matthewvaught <vaught.matthew05@gmail.com>
2025-10-29 21:40:49 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -8,3 +8,7 @@ renderProjects(projects, projectsContainer, 'h2');
 
 const titleElement = document.querySelector('.projects-title');
 titleElement.textContent = `${projects.length} Projects`;
+
+<svg id="projects-pie-plot" viewBox="-50 -50 100 100">
+  <circle cx="0" cy="0" r="50" fill="red" />
+</svg>
