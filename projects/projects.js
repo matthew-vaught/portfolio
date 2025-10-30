a25dced35b9ad87ce88ab8a83d92b8faf9d99725
matthewvaught <vaught.matthew05@gmail.com>
2025-10-29 21:46:00 -0700

    adding circle with js

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 
@@ -9,6 +10,11 @@ renderProjects(projects, projectsContainer, 'h2');
 const titleElement = document.querySelector('.projects-title');
 titleElement.textContent = `${projects.length} Projects`;
 
-<svg id="projects-pie-plot" viewBox="-50 -50 100 100">
-  <path d="M -50 0 A 50 50 0 0 1 50 0 A 50 50 0 0 1 -50 0" fill="red" />
-</svg>
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
