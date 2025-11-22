335f02c193791ce151c8656ef7560a50232fe885
matthewvaught <vaught.matthew05@gmail.com>
2025-11-21 17:15:30 -0800

    refverting to somewhat functional

--- meta/main.js
+++ meta/main.js
@@ -102,20 +102,16 @@ function updateScatter(commits) {
     .attr("fill","steelblue")
     .attr("opacity",.7)
     .on("mouseenter",(e,d)=>{
-      showTooltip(e, d);
+      showTooltip(d);
     })
     .on("mouseleave", hideTooltip);
 }
 
 // ---------- Tooltip ----------
-function showTooltip(e, d){
+function showTooltip(d){
   const t = document.getElementById("commit-tooltip");
-
+  t.hidden = false;
   t.classList.add("visible");
-
-  t.style.left = (e.pageX + 15) + "px";
-  t.style.top  = (e.pageY + 15) + "px";
-
   document.getElementById("commit-link").textContent = d.id;
   document.getElementById("commit-link").href = d.url;
   document.getElementById("commit-date").textContent = d.datetime.toDateString();
